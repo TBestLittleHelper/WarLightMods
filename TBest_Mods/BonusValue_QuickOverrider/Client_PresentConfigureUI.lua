3f190811a5cbd384ac0bbaea065c71e173f05bca
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-09 05:04:54 -0400

    Update Client_PresentConfigureUI.lua

--- TBest_Mods/BonusValue_QuickOverrider/Client_PresentConfigureUI.lua
+++ TBest_Mods/BonusValue_QuickOverrider/Client_PresentConfigureUI.lua
@@ -4,22 +4,31 @@ function Client_PresentConfigureUI(rootParent)
 	local initialNegatives = Mod.Settings.ChangeNegavtive;
 	local initialMultiply = Mod.Settings.ChangeMultiply;
 	local initialBonusValue = Mod.Settings.ChangeDefaultBonus;
+	local initialValueRandom = Mod.Settings.RandomFactor;
 	
 	if initialValue == nil then initialValue = -1; end
 	if initialNegatives == nil then initialNegatives = true; end
 	if initialMultiply == nil then initialMultiply = false; end
     if initialBonusValue == nil then initialBonusValue = false; end
+	if initialValueRandom == nil then initialValueRandom = 0; end
 	
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
-	UI.CreateLabel(vert).SetText("By default the formula used is BonusValue = numberOfTerritories + N");	
-	
+	UI.CreateLabel(vert).SetText("By default the formula used is BonusValue = numberOfTerritories + N ");	
+	UI.CreateLabel(vert).SetText("X randomely changes each bonus value AFTER the formula by any number from - X to + X");
+
 
     local horz = UI.CreateHorizontalLayoutGroup(vert);
 	UI.CreateLabel(horz).SetText('N: ');
     numberInputField = UI.CreateNumberInputField(horz)
-		.SetSliderMinValue(-15)
-		.SetSliderMaxValue(15)
+		.SetSliderMinValue(-10)
+		.SetSliderMaxValue(10)
 		.SetValue(initialValue);
+		
+	UI.CreateLabel(horz).SetText('X: ');
+    numberInputFieldRandomFactor = UI.CreateNumberInputField(horz)
+		.SetSliderMinValue(0)
+		.SetSliderMaxValue(10)
+		.SetValue(initialValueRandom);
 
 	changeNegative = UI.CreateCheckBox(vert).SetText('Change negative bonuses').SetIsChecked(initialNegatives);
 	UI.CreateLabel(vert).SetText("Normally, negative bonuses WILL be modified.  However, you can uncheck the \"Change negative bonuses\" box to preserve bonuses that are 0, or lower.");
@@ -29,6 +38,5 @@ function Client_PresentConfigureUI(rootParent)
 	
 	changeDefaultBonus = UI.CreateCheckBox(vert).SetText('Default bonuse value').SetIsChecked(initialBonusValue);
 	UI.CreateLabel(vert).SetText("Use the default bonuse values. If this is checked, then the multplier uses BonusValue = defaultBonusValue * N. While the default formula is no longer using numberOfTerritories. Instead N is now a flat change, BonusValue = defaultBonusValue + N.");
-	
 	
 end
