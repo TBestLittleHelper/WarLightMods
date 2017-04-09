b2545ec1d5e62f9ab080607c697dff3573dd1195
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-09 00:27:20 -0400

    Update Client_SaveConfigureUI.lua

--- TBest_Mods/BonusValue=NumTerritoriesPlusOrMinusN/Client_SaveConfigureUI.lua
+++ TBest_Mods/BonusValue=NumTerritoriesPlusOrMinusN/Client_SaveConfigureUI.lua
@@ -3,4 +3,5 @@ function Client_SaveConfigureUI(alert)
 	Mod.Settings.Amount = numberInputField.GetValue();
  	Mod.Settings.ChangeNegavtive = changeNegative.GetIsChecked();
 	Mod.Settings.Multiply = changeMultiply.GetIsChecked();
+	Mod.Settings.ChangeDefaultBonus = changeDefaultBonus.GetIsChecked();
 end
