1be0e68bb3ab4889ed6f54257443a2b270901917
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-08 20:21:29 -0400

    Update Client_PresentConfigureUI.lua

--- TBest_Mods/BonusValue=NumTerritoriesPlusOrMinusN/Client_PresentConfigureUI.lua
+++ TBest_Mods/BonusValue=NumTerritoriesPlusOrMinusN/Client_PresentConfigureUI.lua
@@ -2,8 +2,10 @@
 function Client_PresentConfigureUI(rootParent)
 	local initialValue = Mod.Settings.Amount;
 	local initialNegatives = Mod.Settings.ChangeNegavtive;
+	local initialMultiply = Mod.Settings.ChangeMultiply;
 	if initialValue == nil then initialValue = -1; end
 	if initialNegatives == nil then initialNegatives = true; end
+	if initialMultiply == nil then initialMultiply = false; end
     
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
 
@@ -15,7 +17,9 @@ function Client_PresentConfigureUI(rootParent)
 		.SetValue(initialValue);
 
 	UI.CreateLabel(vert).SetText("Normally, negative bonuses WILL be modified.  However, you can uncheck the \"Change negative bonuses\" box to preserve them.");
-
 	changeNegative = UI.CreateCheckBox(vert).SetText('Change negative bonuses').SetIsChecked(initialNegatives);
+	
+	UI.CreateLabel(vert).SetText("Make N work as a multplier");
+	changeNegative = UI.CreateCheckBox(vert).SetText('Multiplier').SetIsChecked(initialNegatives);
 
 end
