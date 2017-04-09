71ad81109898649850c8fedfa00104c077c260f6
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-09 00:26:22 -0400

    Update Server_Created.lua

--- TBest_Mods/BonusValue=NumTerritoriesPlusOrMinusN/Server_Created.lua
+++ TBest_Mods/BonusValue=NumTerritoriesPlusOrMinusN/Server_Created.lua
@@ -5,17 +5,27 @@ function Server_Created(game, settings)
     for _, bonus in pairs(game.Map.Bonuses) do
 		--skip negative bonuses unless AllowNegative was checked
 		if (bonus.Amount > 0 or Mod.Settings.AllowNegative) then 
-			local numTerritories = numTerritories(bonus);
-			local newValue = numTerritories + Mod.Settings.Amount;
-				if (Mod.Settings.Multiply) then
-					newValue = numTerritories * Mod.Settings.Amount;
+			local bonusValue;
+				--if we use defoult Bonuse value, treat it as bonusValue. Else we use numTerritories
+				if (Mod.Settings.ChangeDefaultBonus) then
+					bonusValue = bonus.Amount;
+				else
+					bonusValue = numTerritories(bonus);
 				end
-				
+			local newValue = bonusValue + Mod.Settings.Amount;
+				--if we multiply
+				if (Mod.Settings.Multiply) then
+						newValue = bonusValue * Mod.Settings.Amount;
+				end
+	
 			-- -1000 to +1000 is the maximum allowed range for overridden bonuses, never go beyond that
 			if (newValue < -1000) then newValue = -1000 end;
 			if (newValue > 1000) then newValue = 1000 end;
 		
-			overriddenBonuses[bonus.ID] = newValue;
+			if (newValue ~= bonus.Amount) then --don't do anything if we're not changing the bonus.  We could leave this check off and it would work, but it show up in Settings as an overridden bonus when it's not.
+				overriddenBonuses[bonus.ID] = newValue;
+			end
+
 		end
     end
 
@@ -23,6 +33,7 @@ function Server_Created(game, settings)
 
 end
 
+--Territories in a bonus
 function numTerritories(bonus)
   local count = 0
 	for Territories in pairs(bonus.Territories) do 
