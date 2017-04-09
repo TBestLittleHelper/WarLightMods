d582095c65f4d52fca74ea5bfd99b3ac34ed0a64
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-09 05:04:31 -0400

    Update Client_PresentSettingsUI.lua

--- TBest_Mods/BonusValue_QuickOverrider/Client_PresentSettingsUI.lua
+++ TBest_Mods/BonusValue_QuickOverrider/Client_PresentSettingsUI.lua
@@ -11,12 +11,18 @@ function Client_PresentSettingsUI(rootParent)
 		
 		if (Mod.Settings.Multiply) then
 			Formula = Formula .. '* ';
-		else
-			Formula = Formula ..  '+ ';
+			else if (Mod.Settings.Amount > -1) then --negative numbers get a '-' autmaticaly
+				Formula = Formula ..  '+ ';
+			end
 		end
-	Formula = Formula .. Mod.Settings.Amount;
-	
+		Formula = Formula .. Mod.Settings.Amount;
+		if (Mod.Settings.RandomFactor ~=0) then
+			Formula = Formula .. ' + X'
+		end
+		
 	UI.CreateLabel(vert).SetText(Formula);
+		if (Mod.Settings.RandomFactor ~=0) then --if X is used
+			UI.CreateLabel(vert).SetText('X is any random number from -'.. Mod.Settings.RandomFactor .. ' to ' .. Mod.Settings.RandomFactor);
+		end
 	UI.CreateLabel(vert).SetText('Change negative bonuses: ' .. tostring(Mod.Settings.ChangeNegavtive));
 end
-
