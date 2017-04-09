f52802fd7dda6a126a1f6ff1777191526f41730f
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-09 05:04:10 -0400

    Update Client_SaveConfigureUI.lua

--- TBest_Mods/BonusValue_QuickOverrider/Client_SaveConfigureUI.lua
+++ TBest_Mods/BonusValue_QuickOverrider/Client_SaveConfigureUI.lua
@@ -4,4 +4,5 @@ function Client_SaveConfigureUI(alert)
  	Mod.Settings.ChangeNegavtive = changeNegative.GetIsChecked();
 	Mod.Settings.Multiply = changeMultiply.GetIsChecked();
 	Mod.Settings.ChangeDefaultBonus = changeDefaultBonus.GetIsChecked();
+	Mod.Settings.RandomFactor = math.abs (numberInputFieldRandomFactor.GetValue());
 end
