3ff32a24724cb52466c083b956daadbf22be91fd
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-08 20:39:29 -0400

    Delete Client_PresentSettingsUI.lua

--- TBest_Mods/BonusValue=NumTerritoriesPlusOrMinusN/BonusValue=NumTerritoriesPlusOrMinusN/Client_PresentSettingsUI.lua
+++
@@ -1,9 +0,0 @@
-
-function Client_PresentSettingsUI(rootParent)
-	local vert = UI.CreateVerticalLayoutGroup(rootParent);
-	
-	UI.CreateLabel(vert).SetText('BonusValue = numTerritories +/- ' .. Mod.Settings.Amount);
-	UI.CreateLabel(vert).SetText('Change negative bonuses ' .. Mod.Settings.changeNegative);
-	UI.CreateLabel(vert).SetText('Change multiply ' .. Mod.Settings.changeMultiply);
-end
-
