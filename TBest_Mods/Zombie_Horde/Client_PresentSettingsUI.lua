b31f0ef79ec4bc90686ab6e0c7e4011e06e57507
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-10 14:13:28 -0400

    Update Client_PresentSettingsUI.lua

--- TBest_Mods/Zombie_Horde/Client_PresentSettingsUI.lua
+++ TBest_Mods/Zombie_Horde/Client_PresentSettingsUI.lua
@@ -2,7 +2,8 @@
 function Client_PresentSettingsUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
 
-	UI.CreateLabel(vert).SetText('Zombie Strength: ' .. Mod.Settings.ZombieStrength );
-	UI.CreateLabel(vert).SetText('Zombie Starts: ' .. Mod.Settings.ZombieStarts);
+	UI.CreateLabel(vert).SetText('Zombie Starting Armies on each territory: ' .. Mod.Settings.ZombieStrength );
+	UI.CreateLabel(vert).SetText('Zombie Starting Bonus is worth: ' .. Mod.Settings.ZombieStarts);
+	UI.CreateLabel(vert).SetText('Zombie(s) starts with this number of bonuses : ' .. Mod.Settings.StartingBonuses );
 	UI.CreateLabel(vert).SetText('Zombie Team members: ' .. Mod.Settings.ZombieTeam);
 end
