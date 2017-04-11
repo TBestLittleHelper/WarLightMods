86a9a3047e95b19a3409aa36c5b3ba05ce0e72fd
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-11 19:58:47 -0400

    add superbonus

--- TBest_Mods/BonusValue_QuickOverrider/Client_PresentConfigureUI.lua
+++ TBest_Mods/BonusValue_QuickOverrider/Client_PresentConfigureUI.lua
@@ -5,37 +5,60 @@ function Client_PresentConfigureUI(rootParent)
 	local initialMultiply = Mod.Settings.ChangeMultiply;
 	local initialBonusValue = Mod.Settings.ChangeDefaultBonus;
 	local initialValueRandom = Mod.Settings.RandomFactor;
+	local initialChangeSuperBonuses = Mod.Settings.ChangeSuperBonuses;
+	local initialDefineSuperBonus = Mod.Settings.SuperBonus;
+	local initialSmalSuperBonuses = Mod.Settings.SmalSuperBonuses
 	
 	if initialValue == nil then initialValue = -1; end
-	if initialNegatives == nil then initialNegatives = true; end
+	if initialNegatives == nil then initialNegatives = false; end
 	if initialMultiply == nil then initialMultiply = false; end
  	if initialBonusValue == nil then initialBonusValue = false; end
 	if initialValueRandom == nil then initialValueRandom = 0; end
+	if initialChangeSuperBonuses == nil then initialChangeSuperBonuses = false; end
+	if initialDefineSuperBonus == nil then initialDefineSuperBonus = 15; end
+	if initialSmalSuperBonuses == nil then initialSmalSuperBonuses = false; end
 	
-	local vert = UI.CreateVerticalLayoutGroup(rootParent);
+	local mainContainer = UI.CreateVerticalLayoutGroup(rootParent);
+	
+	local vert = UI.CreateVerticalLayoutGroup(mainContainer);
 	UI.CreateLabel(vert).SetText("By default the formula used is BonusValue = numberOfTerritories + N ");	
 	UI.CreateLabel(vert).SetText("X randomely changes each bonus value AFTER the formula by any number from - X to + X");
-
-    local horz = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(horz).SetText('N: ');
-    numberInputField = UI.CreateNumberInputField(horz)
+	UI.CreateLabel(vert).SetText("T defines the minimum territories for a superbonus. By default superbonuses are NOT changed.");	
+        	
+	local slidersNXT = UI.CreateHorizontalLayoutGroup(mainContainer);
+	UI.CreateLabel(slidersNXT).SetText('N: ');
+    numberInputField = UI.CreateNumberInputField(slidersNXT)
 		.SetSliderMinValue(-5)
 		.SetSliderMaxValue(5)
 		.SetValue(initialValue);
 		
-	UI.CreateLabel(horz).SetText('X: ');
-    numberInputFieldRandomFactor = UI.CreateNumberInputField(horz)
+	UI.CreateLabel(slidersNXT).SetText('X: ');
+    numberInputFieldRandomFactor = UI.CreateNumberInputField(slidersNXT)
 		.SetSliderMinValue(0)
 		.SetSliderMaxValue(10)
 		.SetValue(initialValueRandom);
+		
+	UI.CreateLabel(slidersNXT).SetText('T: ');
+    numberInputFieldDefineSuperBonus = UI.CreateNumberInputField(slidersNXT)
+		.SetSliderMinValue(10)
+		.SetSliderMaxValue(20)
+		.SetValue(initialDefineSuperBonus);
+		
+	
+	local checkBoxses = UI.CreateVerticalLayoutGroup(mainContainer);
+	changeMultiply = UI.CreateCheckBox(checkBoxses).SetText('Multiplier').SetIsChecked(initialMultiply);
+	UI.CreateLabel(checkBoxses).SetText("Make N work as a multplier. The function is then BonusValue = numberOfTerritories * N");
+	
+	changeDefaultBonus = UI.CreateCheckBox(checkBoxses).SetText('Default Bonuse Value').SetIsChecked(initialBonusValue);
+	UI.CreateLabel(checkBoxses).SetText("Use the default bonuse values. If this is checked, then the multplier uses BonusValue = defaultBonusValue * N. While the default formula is no longer using numberOfTerritories. Instead N is now a flat change, BonusValue = defaultBonusValue + N.");
+	
+	local checkBoxsesSuperBonuses = UI.CreateHorizontalLayoutGroup(checkBoxses);
+	ChangeSuperBonuses = UI.CreateCheckBox(checkBoxsesSuperBonuses).SetText('Change Superbonuses').SetIsChecked(initialChangeSuperBonuses);
+	SmalSuperBonuses = UI.CreateCheckBox(checkBoxsesSuperBonuses).SetText('Small Superbonuses').SetIsChecked(initialsmallSuperBonuses);
+	UI.CreateLabel(checkBoxses).SetText("Normally, superbonuses will NOT be modified.");
+	UI.CreateLabel(checkBoxses).SetText("Small Superbonuses sets them to 1, IF Change Superbonuses is also cheked.");
 
-	changeNegative = UI.CreateCheckBox(vert).SetText('Change negative bonuses').SetIsChecked(initialNegatives);
-	UI.CreateLabel(vert).SetText("Normally, negative bonuses WILL be modified.  However, you can uncheck the \"Change negative bonuses\" box to preserve bonuses that are 0, or lower.");
-	
-	changeMultiply = UI.CreateCheckBox(vert).SetText('Multiplier').SetIsChecked(initialMultiply);
-	UI.CreateLabel(vert).SetText("Make N work as a multplier. The function is then BonusValue = numberOfTerritories * N");
-	
-	changeDefaultBonus = UI.CreateCheckBox(vert).SetText('Default bonuse value').SetIsChecked(initialBonusValue);
-	UI.CreateLabel(vert).SetText("Use the default bonuse values. If this is checked, then the multplier uses BonusValue = defaultBonusValue * N. While the default formula is no longer using numberOfTerritories. Instead N is now a flat change, BonusValue = defaultBonusValue + N.");
-	
+	changeNegative = UI.CreateCheckBox(checkBoxses).SetText('Change Negative Bonuses').SetIsChecked(initialNegatives);
+	UI.CreateLabel(checkBoxses).SetText("Normally, negative and 0 bonuses will NOT be modified.");
+
 end
