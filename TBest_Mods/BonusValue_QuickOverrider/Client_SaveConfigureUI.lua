2be85f92209c68b1831b78a44ca07a1ade5c8fa6
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-11 19:56:37 -0400

    add superbonus

--- TBest_Mods/BonusValue_QuickOverrider/Client_SaveConfigureUI.lua
+++ TBest_Mods/BonusValue_QuickOverrider/Client_SaveConfigureUI.lua
@@ -5,4 +5,7 @@ function Client_SaveConfigureUI(alert)
 	Mod.Settings.Multiply = changeMultiply.GetIsChecked();
 	Mod.Settings.ChangeDefaultBonus = changeDefaultBonus.GetIsChecked();
 	Mod.Settings.RandomFactor = math.abs (numberInputFieldRandomFactor.GetValue());
+	Mod.Settings.ChangeSuperBonuses = ChangeSuperBonuses.GetIsChecked();
+	Mod.Settings.SuperBonus = numberInputFieldDefineSuperBonus.GetValue();
+	Mod.Settings.SmalSuperBonuses = SmalSuperBonuses.GetIsChecked();
 end
