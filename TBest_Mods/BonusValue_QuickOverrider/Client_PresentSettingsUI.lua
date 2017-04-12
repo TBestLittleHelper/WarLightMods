18793cab57d12f9ceb1d863af0fa839642e45735
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-11 20:00:30 -0400

    Update Client_PresentSettingsUI.lua

--- TBest_Mods/BonusValue_QuickOverrider/Client_PresentSettingsUI.lua
+++ TBest_Mods/BonusValue_QuickOverrider/Client_PresentSettingsUI.lua
@@ -25,4 +25,10 @@ function Client_PresentSettingsUI(rootParent)
 			UI.CreateLabel(vert).SetText('X is any random number from -'.. Mod.Settings.RandomFactor .. ' to ' .. Mod.Settings.RandomFactor);
 		end
 	UI.CreateLabel(vert).SetText('Change negative bonuses: ' .. tostring(Mod.Settings.ChangeNegavtive));
+	UI.CreateLabel(vert).SetText('Change superbonuses: ' .. tostring(Mod.Settings.ChangeSuperBonuses));
+	UI.CreateLabel(vert).SetText('Superbonuses has at least '.. Mod.Settings.SuperBonus ..' territories');
+	
+	if (Mod.Settings.SmalSuperBonuses == true and Mod.Settings.ChangeSuperBonuses == true) then
+		UI.CreateLabel(vert).SetText('Superbonuses are set to 1 ');
+	end
 end
