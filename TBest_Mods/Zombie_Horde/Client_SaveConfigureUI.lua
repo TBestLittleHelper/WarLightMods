efb8c0876f361395a0c766af03bc556cd6ce75a4
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-19 13:43:21 -0400

    Update Client_SaveConfigureUI.lua

--- TBest_Mods/Zombie_Horde/Client_SaveConfigureUI.lua
+++ TBest_Mods/Zombie_Horde/Client_SaveConfigureUI.lua
@@ -1,7 +1,4 @@
 
 function Client_SaveConfigureUI(alert)
-	Mod.Settings.ZombieStrength = numberInputField.GetValue();
- 	Mod.Settings.ZombieStarts = numberInputZombieStarts.GetValue();
-	Mod.Settings.ZombieTeam= numberInputZombieTeam.GetValue();
-	Mod.Settings.StartingBonuses = numberInputStartingBonuses.GetValue();
+    Mod.Settings.ExtraArmies = numberInputField1.GetValue();
 end
