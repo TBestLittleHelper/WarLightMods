2ee3cbcecd62f66d35d06dd801c4f4c342370137
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-28 15:57:42 -0400

    Update Server_AdvanceTurn.lua

--- TBest_Mods/SimpleDiploMod/Server_AdvanceTurn.lua
+++ TBest_Mods/SimpleDiploMod/Server_AdvanceTurn.lua
@@ -32,7 +32,7 @@ function isAtWar(game, order)
 	if (standing.ActiveCards ~= nill) then --if active cards
 		for _, card in pairs (standing.ActiveCards) do 	
 
-	print (card.Card.CardInstanceID.TargetPlayerID);
+	print (card.Card.CardInstanceID.TargetPlayerIDs);
 		if(card.Card.CardID == GameOrderPlayCardSpy) then --look at spy cards
 			if(card.TargetPlayerID == terrDefender) then	--if we spy on the rigth player			
 				addNewOrder(WL.GameOrderEvent.Create(order.PlayerID,'Is at war with ' .. terrDefender));
