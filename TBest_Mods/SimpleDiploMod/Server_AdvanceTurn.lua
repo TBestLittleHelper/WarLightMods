5d53fe63d385176947fc3c8dad6f6077a10447e7
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-27 22:48:28 -0400

    Update Server_AdvanceTurn.lua

--- TBest_Mods/SimpleDiploMod/Server_AdvanceTurn.lua
+++ TBest_Mods/SimpleDiploMod/Server_AdvanceTurn.lua
@@ -1,3 +1,7 @@
+--TODO
+--Give players 1 card if they have no cards. Prvent stuck games
+--
+
 
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
 	--[[if (order.proxyType == 'GameOrderAttackTransfer' and order.PlayerID == 4569) then
@@ -7,7 +11,7 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
     if (game.Game.NumberOfTurns < Mod.Settings.NumTurns  -- are we at the start of the game, within our defined range?  (without this check, we'd affect the entire game, not just the start)
 		and order.proxyType == 'GameOrderAttackTransfer'  --is this an attack/transfer order?  (without this check, we'd stop deployments or cards)
 		and result.IsAttack  --is it an attack? (without this check, transfers wouldn't be allowed within your own territory or to teammates)
-		and not IsDestinationNeutral(game, order)) then --is the destination owned by neutral? (without this check we'd stop people from attacking neutrals)
+		and not IsDestinationNeutral(game, order) or isAtWar) then --is the destination owned by neutral? (without this check we'd stop people from attacking neutrals)
 		skipThisOrder(WL.ModOrderControl.Skip);
 
 	end
@@ -19,3 +23,17 @@ function IsDestinationNeutral(game, order)
 	local terrOwner = game.ServerGame.LatestTurnStanding.Territories[terrID].OwnerPlayerID; --LatestTurnStanding always shows the current state of the game.
 	return terrOwner == WL.PlayerID.Neutral;
 end
+
+function isAtWar(game, order)
+	local TOterrID = order.To; --The order has "To" and "From" which are territory IDs
+	local FromTerrID = order.From; 
+	local terrDefender = game.ServerGame.LatestTurnStanding.Territories[TOterrID].OwnerPlayerID; --LatestTurnStanding always shows the current state of the game.
+	local terrAttacker = game.ServerGame.LatestTurnStanding.Territories[FromterrID].OwnerPlayerID; --LatestTurnStanding always shows the current state of the game.
+	for(WL.CardID.Spy in ActiveCard) do
+		if(TargetPlayerID = terrAttacker) then
+			return true;
+		end
+	end
+	
+	return false;
+		
