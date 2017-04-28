bdcc682e51d1887898375d92d33b873a6f1e2fcd
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-28 10:14:26 -0400

    Update Server_AdvanceTurn.lua

--- TBest_Mods/SimpleDiploMod/Server_AdvanceTurn.lua
+++ TBest_Mods/SimpleDiploMod/Server_AdvanceTurn.lua
@@ -33,7 +33,7 @@ function isAtWar(game, order)
 	local terrDefender = game.ServerGame.LatestTurnStanding.Territories[TOterrID].OwnerPlayerID; --The player defending
 --attempts to index nil error	local terrAttacker = game.ServerGame.LatestTurnStanding.Territories[FromterrID].OwnerPlayerID; --The player attacking
 	print ("running_isAtWar");
-	for _, GameOrderPlayCard in pairs (game.ServerGame.LatestTurnStanding.ActiveCard) do
+	for _, GameOrderPlayCard in pairs (game.ActiveCard) do
 --		print (GameOrderPlayCard);
 --		if(GameOrderPlayCard == GameOrderPlayCardSpy) then
 --			if(GameOrderPlayCard.TargetPlayerID == terrDefender) then
