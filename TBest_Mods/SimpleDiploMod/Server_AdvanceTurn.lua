51d50a125430bac0ce52f23c03d666be1095793a
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-28 14:04:02 -0400

    Update Server_AdvanceTurn.lua

--- TBest_Mods/SimpleDiploMod/Server_AdvanceTurn.lua
+++ TBest_Mods/SimpleDiploMod/Server_AdvanceTurn.lua
@@ -8,8 +8,9 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 		and result.IsAttack  --is it an attack? (without this check, transfers wouldn't be allowed within your own territory or to teammates)
 		and not IsDestinationNeutral(game, order)) then --is the destination owned by neutral? (without this check we'd stop people from attacking neutrals)
 			if (isAtWar(game, order) == false) then --not at war? skip the attack
+				local terrDefender = game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID;
 				skipThisOrder(WL.ModOrderControl.Skip);
-				addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'You are not at war with the owner of ' .. order.To, {}));
+				addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'You are not at war with ' .. terrDefender.Name, {}));
 			end
 
 	end
