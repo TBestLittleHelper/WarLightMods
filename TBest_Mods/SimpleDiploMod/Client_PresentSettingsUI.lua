f05c7e5b1a9c9ef4f0123939ca682bf2b8614594
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-28 10:07:01 -0400

    Update Client_PresentSettingsUI.lua

--- TBest_Mods/SimpleDiploMod/Client_PresentSettingsUI.lua
+++ TBest_Mods/SimpleDiploMod/Client_PresentSettingsUI.lua
@@ -1,5 +1,5 @@
 function Client_PresentSettingsUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
 	
-	UI.CreateLabel(vert).SetText('Cannot attack other players for the first ' .. Mod.Settings.NumTurns .. ' turns');
+	UI.CreateLabel(vert).SetText('Can attack other players AFTER ' .. Mod.Settings.NumTurns .. ' turns without declering war');
 end
