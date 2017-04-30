eced7cad5d3c54887d38f31c0517368e01c442f0
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-30 17:47:00 -0400

    Update Server_AdvanceTurn.lua

--- TBest_Mods/Zombie_Horde/Server_AdvanceTurn.lua
+++ TBest_Mods/Zombie_Horde/Server_AdvanceTurn.lua
@@ -6,8 +6,7 @@ function Server_AdvanceTurn_Start(game,addNewOrder)
 	local newExtraDeploy = Mod.Settings.ExtraArmies;
 	for _,territory in pairs(standing.Territories) do 		
 		if (territory.OwnerPlayerID == 1) then -- AI 1, in game
-			print (territory.NumArmies.NumArmies .." " .. territory.Name);
-				--TODO determine a better cap
+		--TODO determine a better cap
 			if (newExtraDeploy + territory.NumArmies < newExtraDeploy *10) then
 				if (newExtraDeploy < 0) then newExtraDeploy = 0 end;	
 				if (newExtraDeploy > 1000) then newExtraDeploy = 1000 end;	
