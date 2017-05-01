ed3db5357a95c49193eaf92f470f2cc3a5b9f5a7
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-30 20:52:31 -0400

    Update and rename renameMe to Server_Created.lua

---
+++ TBest_Mods/LottoMod/Server_Created.lua
@@ -0,0 +1,19 @@
+function Server_Created(game, settings)
+--TODO change state of non-winners
+
+  local playersSet = {}
+	  for _, territory in pairs(standing.Territories) do
+		  if (not territory.IsNeutral) then
+			  playersSet[territory.OwnerPlayerID] = true
+  		end
+	  end
+	
+  	local playersTable = {}
+	  local n = 0;
+	  for key, _ in pairs(playersSet) do
+		  playersTable[n] = key
+		  n = n + 1;
+	  end	
+  local winner = math.random(0,n);
+	
+end
