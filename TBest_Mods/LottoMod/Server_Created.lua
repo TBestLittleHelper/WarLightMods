a357a361eb8f8a275eb6cfb0deb4597ecbb03b77
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-01 12:46:19 -0400

    Update Server_Created.lua

--- TBest_Mods/LottoMod/Server_Created.lua
+++ TBest_Mods/LottoMod/Server_Created.lua
@@ -17,3 +17,7 @@ function Server_Created(game, settings)
   local winner = math.random(0,n);
 	
 end
+
+function PrintProxyInfo(obj)
+   print('type=' .. obj.proxyType .. ' readOnly=' .. tostring(obj.readonly) .. ' readableKeys=' .. table.concat(obj.readableKeys, ',') .. ' writableKeys=' .. table.concat(obj.writableKeys, ','));
+end
