babd4669d1e7ef3e992ee2473ce14dd505c8ff0f
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-01 12:56:29 -0400

    Update Server_AdvanceTurn.lua

--- TBest_Mods/Zombie_Horde/Server_AdvanceTurn.lua
+++ TBest_Mods/Zombie_Horde/Server_AdvanceTurn.lua
@@ -22,8 +22,7 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 	standing = game.ServerGame.LatestTurnStanding;
 	local playersAlive;
 
-	--addOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,"Pestilence",{},{terrMod}));
-	addNewOrder(WL.GameOrderEvent.Create(order.PlayerID,'Is  ' .. order.PlayerID,{},{order.To}));
+	addNewOrder(WL.GameOrderEvent.Create(order.PlayerID,'Is  ' .. order.PlayerID,{},));
 --need Update from FIzzer on to see when a state of a player changes
 end
 
