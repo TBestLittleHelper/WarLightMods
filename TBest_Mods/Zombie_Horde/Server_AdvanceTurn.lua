efcbe6d731f1152ef43aacfc680133d46f3bdfad
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-30 21:41:04 -0400

    Update Server_AdvanceTurn.lua

--- TBest_Mods/Zombie_Horde/Server_AdvanceTurn.lua
+++ TBest_Mods/Zombie_Horde/Server_AdvanceTurn.lua
@@ -1,14 +1,10 @@
---TODO Hardcode Zombie surrender when one non-zombie player ramians
---TODO Better way of picking Zombie
+--TODO Hardcode Zombie surrender when one non-zombie player ramians. Wait for FizzerUpdate
+--TODO Better way of picking Zombie?
 
 function Server_AdvanceTurn_Start(game,addNewOrder)
 	standing = game.ServerGame.LatestTurnStanding;
-	print (Mod.Settings.ZombieID);
-	
-	
 	local newExtraDeploy = Mod.Settings.ExtraArmies;
 	for _,territory in pairs(standing.Territories) do 	
-		print (territory.OwnerPlayerID);
 		if (territory.OwnerPlayerID == Mod.Settings.ZombieID) then
 			if (newExtraDeploy + territory.NumArmies.NumArmies < newExtraDeploy *10) then
 				if (newExtraDeploy < 0) then newExtraDeploy = 0 end;	
@@ -18,4 +14,11 @@ function Server_AdvanceTurn_Start(game,addNewOrder)
 			end
 		end
 	end
+end
+
+Server_AdvanceTurn_Order(game,GameOrder,GameOrderResult)
+	standing = game.ServerGame.LatestTurnStanding;
+	local playersAlive;
+	
+--need Update fromFIzzer on GameOrderResult: The result of the order being processed. This is writable, so mods can change the result. Currently, only GameOrderAttackTransferResult has writable fields.
 end
