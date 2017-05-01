0f4931a79aba96285724ed35196d3069162588c8
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-30 21:05:43 -0400

    Update Client_PresentConfigureUI.lua

--- TBest_Mods/Zombie_Horde/Client_PresentConfigureUI.lua
+++ TBest_Mods/Zombie_Horde/Client_PresentConfigureUI.lua
@@ -2,7 +2,7 @@
 function Client_PresentConfigureUI(rootParent)
 	local initialValue1 = Mod.Settings.ExtraArmies;
 	if initialValue1 == nil then initialValue1 = 5; end
-    
+    	if initialZombieID == nil then initialZombieID = 69603; end
 
     local horz1 = UI.CreateHorizontalLayoutGroup(rootParent);
 	UI.CreateLabel(horz1).SetText('Extra Armies for Zombie in EACH territory per Turn:');
@@ -10,6 +10,9 @@ function Client_PresentConfigureUI(rootParent)
 		.SetSliderMinValue(0)
 		.SetSliderMaxValue(10)
 		.SetValue(initialValue1);
-
+	
+	UI.CreateLabel(horz1).SetText('The PlayerID of the Zombie:');
+    numberInputField1 = UI.CreateNumberInputField(horz1)
+		.SetValue(initialValue1);
 
 end
