9fbc9a8ade411041c911960e71b8f56d02f6b4de
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-04-30 20:38:26 -0400

    Update Server_AdvanceTurn.lua

--- TBest_Mods/Zombie_Horde/Server_AdvanceTurn.lua
+++ TBest_Mods/Zombie_Horde/Server_AdvanceTurn.lua
@@ -1,12 +1,11 @@
 --TODO Hardcode Zombie surrender when one non-zombie player ramians
---TODO cap deploy
+--TODO Remove hardcode Zombie on AI 1
 
 function Server_AdvanceTurn_Start(game,addNewOrder)
 	standing = game.ServerGame.LatestTurnStanding;
 	local newExtraDeploy = Mod.Settings.ExtraArmies;
 	for _,territory in pairs(standing.Territories) do 		
 		if (territory.OwnerPlayerID == 1) then -- AI 1, in game
-		--TODO determine a better cap
 			if (newExtraDeploy + territory.NumArmies.NumArmies < newExtraDeploy *10) then
 				if (newExtraDeploy < 0) then newExtraDeploy = 0 end;	
 				if (newExtraDeploy > 1000) then newExtraDeploy = 1000 end;	
