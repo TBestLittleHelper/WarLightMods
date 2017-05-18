58dd65b4d98ff44fdc9cfdb3683030cd523dfb41
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-18 12:17:30 +0200

    Create Utilities.lua

---
+++ TBest_Mods/SimpleDiploMod/Utilities.lua
@@ -0,0 +1,76 @@
+function Dump(obj)
+	if obj.proxyType ~= nil then
+		DumpProxy(obj);
+	elseif type(obj) == 'table' then
+		DumpTable(obj);
+	else
+		print('Dump ' .. type(obj));
+	end
+end
+function DumpTable(tbl)
+    for k,v in pairs(tbl) do
+        print('k = ' .. tostring(k) .. ' (' .. type(k) .. ') ' .. ' v = ' .. tostring(v) .. ' (' .. type(v) .. ')');
+    end
+end
+function DumpProxy(obj)
+
+    print('type=' .. obj.proxyType .. ' readOnly=' .. tostring(obj.readonly) .. ' readableKeys=' .. table.concat(obj.readableKeys, ',') .. ' writableKeys=' .. table.concat(obj.writableKeys, ','));
+end
+
+function split(str, pat)
+   local t = {}  -- NOTE: use {n = 0} in Lua-5.0
+   local fpat = "(.-)" .. pat
+   local last_end = 1
+   local s, e, cap = str:find(fpat, 1)
+   while s do
+      if s ~= 1 or cap ~= "" then
+         table.insert(t,cap)
+      end
+      last_end = e+1
+      s, e, cap = str:find(fpat, last_end)
+   end
+   if last_end <= #str then
+      cap = str:sub(last_end)
+      table.insert(t, cap)
+   end
+   return t
+end
+
+
+function map(array, func)
+	local new_array = {}
+	local i = 1;
+	for _,v in pairs(array) do
+		new_array[i] = func(v);
+		i = i + 1;
+	end
+	return new_array
+end
+
+
+function filter(array, func)
+	local new_array = {}
+	local i = 1;
+	for _,v in pairs(array) do
+		if (func(v)) then
+			new_array[i] = v;
+			i = i + 1;
+		end
+	end
+	return new_array
+end
+
+function first(array, func)
+	for _,v in pairs(array) do
+		if (func(v)) then
+			return v;
+		end
+	end
+	return nil;
+end
+
+function randomFromArray(array)
+	local len = #array;
+	local i = math.random(len);
+	return array[i];
+end
