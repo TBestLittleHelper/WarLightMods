b45d5d778eabe9301f121b2ba21d513f06f4edbc
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-18 13:20:56 +0200

    Update Client_PresentMenuUI.lua

--- TBest_Mods/SimpleDiploMod/Client_PresentMenuUI.lua
+++ TBest_Mods/SimpleDiploMod/Client_PresentMenuUI.lua
@@ -15,14 +15,7 @@ function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game)
 	local row1 = UI.CreateHorizontalLayoutGroup(vert);
 	UI.CreateLabel(row1).SetText("Decler war on this player: ");
 	TargetPlayerBtn = UI.CreateButton(row1).SetText("Select player...").SetOnClick(TargetPlayerClicked);
-
-
-	local row2 = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(row2).SetText("Gift armies from this territory: ");
-	TargetTerritoryBtn = UI.CreateButton(row2).SetText("Select source territory...").SetOnClick(TargetTerritoryClicked);
-
 end
-
 
 function TargetPlayerClicked()
 	local options = map(Game.Game.Players, PlayerButton);
@@ -45,20 +38,18 @@ function CheckCreateFinalStep()
 	if (SubmitBtn == nil) then
 
 		local row3 = UI.CreateHorizontalLayoutGroup(vert);
-		UI.CreateLabel(row3).SetText("How many armies would you like to gift: ");
-		NumArmiesInput = UI.CreateNumberInputField(row3).SetSliderMinValue(1);
+		UI.CreateLabel(row3).SetText("How many turns would you like to declere war for?");
+		NumTurnInput = UI.CreateNumberInputField(row3).SetSliderMinValue(1);
 
-		SubmitBtn = UI.CreateButton(vert).SetText("Gift").SetOnClick(SubmitClicked);
+		SubmitBtn = UI.CreateButton(vert).SetText("Declere War").SetOnClick(SubmitClicked);
 	end
-
-	local maxArmies = Game.LatestStanding.Territories[TargetTerritoryID].NumArmies.NumArmies;
-	NumArmiesInput.SetSliderMaxValue(maxArmies).SetValue(maxArmies);
+	NumArmiesInput.SetSliderMaxValue(10).SetValue(5);
 end
 
 function SubmitClicked()
 	local msg = 'Declere War on ' .. Game.Game.Players[TargetPlayerID].DisplayName(nil, false);
 
-	local payload = NumArmiesInput.GetValue() .. ',' .. TargetTerritoryID .. ',' .. TargetPlayerID;
+	local payload = NumTurnInput.GetValue() .. ',' .. TargetPlayerID;
 
 	local orders = Game.Orders;
 	table.insert(orders, WL.GameOrderCustom.Create(Game.Us.ID, msg, payload));
