a4ae0879f1cc7981f5ea009f25c12361608aeba5
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-18 15:41:45 +0200

    Update Server_AdvanceTurn.lua

--- TBest_Mods/SimpleDiploMod/Server_AdvanceTurn.lua
+++ TBest_Mods/SimpleDiploMod/Server_AdvanceTurn.lua
@@ -24,7 +24,8 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 		local targetPlayerID = tonumber(payloadSplit[2]);
 		local cardInstanceID = order.PlayerID .. targetPlayerID .. game.Game.NumberOfTurns;
 		--create a spy card, then play it
-		
+--WL.GameOrderReceiveCard.Create(cardID Array<CardID>, playerID PlayerID, instancesCreated Array<CardInstance>) (static) returns GameOrderReceiveCard:
+
 		addNewOrder(WL.GameOrderReceiveCard.Create(WL.CardID.Spy, order.PlayerID, cardInstanceID));
 		addNewOrder(WL.GameOrderPlayCardSpy.Create(cardInstanceID, order.PlayerID, targetPlayerID));
 		skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage); --we replaced the GameOrderCustom with a GameOrderEvent,
