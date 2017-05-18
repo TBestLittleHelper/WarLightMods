f91822506995363f0a9f80c737dd7ef51cab1a47
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-18 21:46:49 +0200

    Update Server_Created.lua

--- TBest_Mods/LottoMod/Server_Created.lua
+++ TBest_Mods/LottoMod/Server_Created.lua
@@ -4,19 +4,20 @@ function Server_Created(game, settings)
 	
 	local Player = game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID;
 	local winner = lucky();
+	
 	for _,territory in pairs(game.ServerGame.LatestTurnStanding.Territories)do
 		territory.OwnerPlayerID = winner;
-		end
 	end
+	
 end
 
 function lucky()
 	local playersTable = {}
 	local n = 0;
-	  for key, _ in pairs(playersSet) do
-		  playersTable[n] = key
-		  n = n + 1;
-	  end	
+	for key, _ in pairs(playersSet) do
+		playersTable[n] = key
+		n = n + 1;
+	end	
   	local winner = math.random(0,n);
 	return winner;
 end
