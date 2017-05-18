8b6c0d25d69f80acd964a45c2f209d6a35bc7f52
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-18 11:55:57 +0200

    Update Server_AdvanceTurn.lua

--- TBest_Mods/SimpleDiploMod/Server_AdvanceTurn.lua
+++ TBest_Mods/SimpleDiploMod/Server_AdvanceTurn.lua
@@ -1,8 +1,8 @@
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
 	standing = game.ServerGame.LatestTurnStanding;
 
-	if (game.Game.NumberOfTurns < Mod.Settings.NumTurns  -- are we at the start of the game, within our defined range?  (without this check, we'd affect the entire game, not just the start)
-		and order.proxyType == 'GameOrderAttackTransfer'  --is this an attack/transfer order?  (without this check, we'd stop deployments or cards)
+	if (game.Game.NumberOfTurns < Mod.Settings.NumTurns) then  -- are we at the start of the game, within our defined range?  (without this check, we'd affect the entire game, not just the start)
+		if ( order.proxyType == 'GameOrderAttackTransfer'  --is this an attack/transfer order?  (without this check, we'd stop deployments or cards)
 		and result.IsAttack  --is it an attack? (without this check, transfers wouldn't be allowed within your own territory or to teammates)
 		and not IsDestinationNeutral(game, order)) then --is the destination owned by neutral? (without this check we'd stop people from attacking neutrals)
 			if (isAtWar(game, order) == false) then --not at war? skip the attack
@@ -10,14 +10,18 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 				skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage);
 			end
 	end
-	if (game.Game.NumberOfTurns < Mod.Settings.NumTurns  -- are we at the start of the game, within our defined range?  (without this check, we'd affect the entire game, not just the start)
-		and order.proxyType == 'GameOrderPlayCardSpy') then
-		addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, order.PlayerID .. ' is now at war with ' .. order.TargetPlayerID, nil)); --Order is public	
-		local playerOne = order.PlayerID;
-		local playerTwo = order.TargetPlayerID;
-		local turnsLeftOfWar = 3; --for now harcode to  3 turns
-		addNewOrder(WL.GameOrderCustom.Create(order.PlayerID, 'Mod System Order. DO NOT DELETE THIS ORDER',playerOne, playerTwo, turnsLeftOfWar));
-	end
+		if (order.proxyType == 'GameOrderPlayCardSpy') then
+			addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, order.PlayerID .. ' is now at war with ' .. order.TargetPlayerID, nil)); --Order is public	
+			local playerOne = order.PlayerID;
+			local playerTwo = order.TargetPlayerID;
+			local turnsLeftOfWar = 3; --for now harcode to  3 turns
+			addNewOrder(WL.GameOrderCustom.Create(order.PlayerID, 'Mod System Order. DO NOT DELETE THIS ORDER',playerOne, playerTwo, turnsLeftOfWar));
+		end
+		
+		if (order.proxyType == 'GameOrderCustom' 
+		and turnsLeftOfWar == 3) then
+			addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, order.PlayerID .. ' is no longer at war with ' .. order.TargetPlayerID, nil)); --Order is public	
+		end
 end
 
 function IsDestinationNeutral(game, order)
