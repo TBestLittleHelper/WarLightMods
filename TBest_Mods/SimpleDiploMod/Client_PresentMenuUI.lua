b57162b5ff26d735aa670fee6d32b5a2572f0e5f
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-18 15:03:33 +0200

    Update Client_PresentMenuUI.lua

--- TBest_Mods/SimpleDiploMod/Client_PresentMenuUI.lua
+++ TBest_Mods/SimpleDiploMod/Client_PresentMenuUI.lua
@@ -53,6 +53,7 @@ function SubmitClicked()
 	local msg = 'Declered War on ' .. Game.Game.Players[TargetPlayerID].DisplayName(nil, false) .. ' for ' .. NumTurnInput.GetValue() .. ' turns';
 
 	local payload = NumTurnInput.GetValue() .. ',' .. TargetPlayerID;
+	print (payload);
 	local orders = Game.Orders;
 	table.insert(orders, WL.GameOrderCustom.Create(Game.Us.ID, msg, payload));
 	Game.Orders = orders;
