1686373c94a9d4b2d3e59dc855f27e9ff53f5664
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-18 12:36:07 +0200

    Update Client_PresentSettingsUI.lua

--- TBest_Mods/SimpleDiploMod/Client_PresentSettingsUI.lua
+++ TBest_Mods/SimpleDiploMod/Client_PresentSettingsUI.lua
@@ -2,5 +2,5 @@ function Client_PresentSettingsUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
 	
 	UI.CreateLabel(vert).SetText('This mod will only affect the game until turn ' .. Mod.Settings.NumTurns);
-	UI.CreateLabel(vert).SetText('Turs from declering war until you can attack is ' .. Mod.Settings.NumTurns);
+	UI.CreateLabel(vert).SetText('Turs from declering war until you can attack is ' .. Mod.Settings.TimeToWar);
 end
