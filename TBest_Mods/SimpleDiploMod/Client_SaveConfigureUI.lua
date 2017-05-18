b0228bc52f8934cf269007b3cd10303e71c9559c
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-18 12:35:11 +0200

    Update Client_SaveConfigureUI.lua

--- TBest_Mods/SimpleDiploMod/Client_SaveConfigureUI.lua
+++ TBest_Mods/SimpleDiploMod/Client_SaveConfigureUI.lua
@@ -1,4 +1,4 @@
 function Client_SaveConfigureUI(alert)
-    
-    Mod.Settings.NumTurns = numberInputField.GetValue();
+    Mod.Settings.NumTurns = numberInputField.GetValue(); --turns the mod is active. After it is an FFA.
+    Mod.Settings.TimeToWar = timeToWarInputField.GetValue(); --turns to declere war. Typicaly 2.
 end
