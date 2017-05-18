790ec9ee786f0fa408efbac4ba5200afeb67491d
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-18 12:35:08 +0200

    Update Client_PresentConfigureUI.lua

--- TBest_Mods/SimpleDiploMod/Client_PresentConfigureUI.lua
+++ TBest_Mods/SimpleDiploMod/Client_PresentConfigureUI.lua
@@ -1,15 +1,25 @@
 
 function Client_PresentConfigureUI(rootParent)
 	local turnsInitial = Mod.Settings.NumTurns;
+	local timeToWarInitial = Mod.Settings.TimeToWar;
+	
 	if turnsInitial == nil then turnsInitial = 10; end
-    
+    	if timeToWarInitial == nile then timeToWarInitial = 0; end
+	
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
-
-    local horz = UI.CreateHorizontalLayoutGroup(vert);
+	
+	local horz = UI.CreateHorizontalLayoutGroup(vert);
 	UI.CreateLabel(horz).SetText("This mod will only be active until turn");
     numberInputField = UI.CreateNumberInputField(horz)
 		.SetSliderMinValue(50)
 		.SetSliderMaxValue(100)
 		.SetValue(turnsInitial);
+	
+	local horz1 = UI.CreateHorizontalLayoutGroup(vert);
+	UI.CreateLabel(horz1).SetText("Turns from decering war, until you can attack");
+	numberInputField = UI.timeToWarInputField(horz1)
+		.SetSliderMinValue(0)
+		.SetSliderMaxValue(5)
+		.SetValue(timeToWarInitial);
 
 end
