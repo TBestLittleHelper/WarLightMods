4f4ee7d4362da04ed2fbd4c8e2cb5e3c499ca6b3
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-18 09:38:58 +0200

    Update Server_AdvanceTurn.lua

--- TBest_Mods/SimpleDiploMod/Server_AdvanceTurn.lua
+++ TBest_Mods/SimpleDiploMod/Server_AdvanceTurn.lua
@@ -19,7 +19,7 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 			end
 	end
 	 if (game.Game.NumberOfTurns < Mod.Settings.NumTurns  -- are we at the start of the game, within our defined range?  (without this check, we'd affect the entire game, not just the start)
-		and order.proxyType == 'GameOrderPlayCardSpy' then  --look at spycard
+		and order.proxyType == 'GameOrderPlayCardSpy') then  --look at spycard
 		addNewOrder(WL.GameOrderEvent.Create(order.PlayerID,' Declered war with ' .. terrDefender));		
 	
 end
