0471c809a766ccdf8781295f49c5349577d17de3
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-20 22:24:51 +0200

    Update Client_PresentSettingsUI.lua

--- TBest_Mods/Zombie_Horde/Client_PresentSettingsUI.lua
+++ TBest_Mods/Zombie_Horde/Client_PresentSettingsUI.lua
@@ -3,5 +3,6 @@ function Client_PresentSettingsUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
 	UI.CreateLabel(vert).SetText('Armies added on EACH Zombie territory each turn:' .. Mod.Settings.ExtraArmies);
 	UI.CreateLabel(vert).SetText('The ZombieID is ' .. Mod.Settings.ZombieID);
+	UI.CreateLabel(vert).SetText('RandomZombie is set to ' .. Mod.Settings.RandomZombie);	
 end
 
