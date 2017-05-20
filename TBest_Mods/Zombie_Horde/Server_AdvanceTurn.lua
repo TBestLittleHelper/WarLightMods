43a4f479e76eb391786b73de39cd508e0e91d432
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-20 11:02:03 +0200

    Update Server_AdvanceTurn.lua

--- TBest_Mods/Zombie_Horde/Server_AdvanceTurn.lua
+++ TBest_Mods/Zombie_Horde/Server_AdvanceTurn.lua
@@ -16,7 +16,7 @@ function Server_AdvanceTurn_End(game,addNewOrder) --Give Zoombie armies at the e
 --the order66 is a modefication from https://github.com/dabo123148/WarlightMod/blob/master/Pestilence/Server_AdvanceTurn.lua
 			end
 		end
-		addOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral,"Cure Found and zombies are now harmless",nil,Order66));
+		addOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral,"Cure Found and zombies are now harmless",{},Order66));
 	else	
 		for _,territory in pairs(standing.Territories) do 	
 			if (territory.OwnerPlayerID == Mod.Settings.ZombieID) then
