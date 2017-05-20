ab20af2bfed040c6e791a08835ba43acc6ec049b
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-20 22:23:59 +0200

    Update Client_PresentConfigureUI.lua

--- TBest_Mods/Zombie_Horde/Client_PresentConfigureUI.lua
+++ TBest_Mods/Zombie_Horde/Client_PresentConfigureUI.lua
@@ -3,23 +3,29 @@
 function Client_PresentConfigureUI(rootParent)
 	local initialValue1 = Mod.Settings.ExtraArmies;
 	local initialZombieID = Mod.Settings.ZombieID;
+	local initialRandomZombie = Mod.Settings.RandomZombie;
 	
 	if initialValue1 == nil then initialValue1 = 5; end
     	if initialZombieID == nil then initialZombieID = 69603; end
-
-	local mainContainer = UI.CreateVerticalLayoutGroup(rootParent);
-
+	if initialRandomZombie == nil then initialRandomZombie = false; end
 	
-    local vert1 = UI.CreateHorizontalLayoutGroup(mainContainer);
+	local mainContainer = UI.CreateVerticalLayoutGroup(rootParent);
+	
+	local vert1 = UI.CreateHorizontalLayoutGroup(mainContainer);
 	UI.CreateLabel(vert1).SetText('Extra Armies for Zombie in EACH territory per Turn:');
-    numberInputField1 = UI.CreateNumberInputField(vert1)
+	numberInputField1 = UI.CreateNumberInputField(vert1)
 		.SetSliderMinValue(0)
 		.SetSliderMaxValue(10)
 		.SetValue(initialValue1);
 
-    local vert2 = UI.CreateHorizontalLayoutGroup(rootParent);	
+	local vert2 = UI.CreateHorizontalLayoutGroup(mainContainer);	
 	UI.CreateLabel(vert2).SetText('The PlayerID of the Zombie:');
-    zombieInputField = UI.CreateNumberInputField(vert2)
+	zombieInputField = UI.CreateNumberInputField(vert2)
 		.SetValue(initialZombieID);
+	
+	local vert3 = UI.CreateHorizontalLayoutGroup(mainContainer);
+	UI.CreateLabel(vert3).SetText('Check this and a random player is turned into a Zombie');
+	local checkBoxses = UI.CreateVerticalLayoutGroup(mainContainer);
+	initialRandomZombie = UI.CreateCheckBox(checkBoxses).SetText('RandomZombie').SetIsChecked(initialRandomZombie);
 
 end
