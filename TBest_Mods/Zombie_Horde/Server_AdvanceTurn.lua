84a2348d8ac7bfbd561fcac477ba5efaae300736
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-20 11:16:32 +0200

    Update Server_AdvanceTurn.lua

--- TBest_Mods/Zombie_Horde/Server_AdvanceTurn.lua
+++ TBest_Mods/Zombie_Horde/Server_AdvanceTurn.lua
@@ -16,19 +16,18 @@ function Server_AdvanceTurn_End(game,addNewOrder) --Give Zoombie armies at the e
 --the order66 is a modefication from https://github.com/dabo123148/WarlightMod/blob/master/Pestilence/Server_AdvanceTurn.lua
 			end
 		end
-	else	
-		for _,territory in pairs(standing.Territories) do 	
-			if (territory.OwnerPlayerID == Mod.Settings.ZombieID) then
-				if (newExtraDeploy + territory.NumArmies.NumArmies < newExtraDeploy *10) then
-					if (newExtraDeploy < 0) then newExtraDeploy = 0 end;	
-					if (newExtraDeploy > 1000) then newExtraDeploy = 1000 end;	
+	addOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral,"Cure Found and zombies are now harmless",nil,Order66));
+	end
+	for _,territory in pairs(standing.Territories) do 	
+		if (territory.OwnerPlayerID == Mod.Settings.ZombieID) then
+			if (newExtraDeploy + territory.NumArmies.NumArmies < newExtraDeploy *10) then
+				if (newExtraDeploy < 0) then newExtraDeploy = 0 end;	
+				if (newExtraDeploy > 1000) then newExtraDeploy = 1000 end;	
 --make this only add one event, that is "All Zombie territories deployex X armies. ?
 				addNewOrder(WL.GameOrderDeploy.Create(Mod.Settings.ZombieID, newExtraDeploy, territory.ID,nil,GameOrderDeploy));
-				end
 			end
 		end
 	end
-	addOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral,"Cure Found and zombies are now harmless",nil,Order66));
 end
 
 function playersAlive()
