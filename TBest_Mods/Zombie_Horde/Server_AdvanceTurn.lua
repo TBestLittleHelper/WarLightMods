cf978e68246f2e8a282733a8cdc164473029c79b
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-22 11:59:48 +0200

    Update Server_AdvanceTurn.lua

--- TBest_Mods/Zombie_Horde/Server_AdvanceTurn.lua
+++ TBest_Mods/Zombie_Horde/Server_AdvanceTurn.lua
@@ -1,13 +1,13 @@
---TODO Better way of picking Zombie? Then inpuPlayerID. Random Zombie, AI Zombie. Determine ZombieID on server created and store as Mod.Settings.ZombieID
-
 function Server_AdvanceTurn_End(game,addNewOrder) --Give Zoombie armies at the end of a turn
 	standing = game.ServerGame.LatestTurnStanding;
 	local newExtraDeploy = Mod.Settings.ExtraArmies;
 	CurrentIndex=1;
 	Order66={};
+	local ZombieID = ZombieID(Mod.Settings.RandomSeed);
+	
 	if (playersAlive() == 2) then --update to count teams, not players
 		for _,territory in pairs(standing.Territories) do 
-			if (territory.OwnerPlayerID == Mod.Settings.ZombieID) then
+			if (territory.OwnerPlayerID == ZombieID) then
 				terrMod = WL.TerritoryModification.Create(territory.ID);
 				terrMod.SetOwnerOpt=WL.PlayerID.Neutral;
 				Order66[CurrentIndex]=terrMod;
@@ -18,16 +18,42 @@ function Server_AdvanceTurn_End(game,addNewOrder) --Give Zoombie armies at the e
 	addNewOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral,"Cure Found and zombies are now harmless",nil,Order66));
 	else
 		for _,territory in pairs(standing.Territories) do 	
-			if (territory.OwnerPlayerID == Mod.Settings.ZombieID) then
+			if (territory.OwnerPlayerID == ZombieID) then
 				if (newExtraDeploy + territory.NumArmies.NumArmies < newExtraDeploy *10) then
 					if (newExtraDeploy < 0) then newExtraDeploy = 0 end;	
 					if (newExtraDeploy > 1000) then newExtraDeploy = 1000 end;	
 --make this only add one event, that is "All Zombie territories deployex X armies. ?
-					addNewOrder(WL.GameOrderDeploy.Create(Mod.Settings.ZombieID, newExtraDeploy, territory.ID,nil,GameOrderDeploy));
+					addNewOrder(WL.GameOrderDeploy.Create(ZombieID, newExtraDeploy, territory.ID,nil,GameOrderDeploy));
 				end
 			end
 		end
 	end
+end
+
+function ZombieID(seed)
+	local playersSet = {}
+	for _, territory in pairs(game.TurnZeroStanding ) do
+		if (not territory.IsNeutral) then
+			playersSet[territory.OwnerPlayerID] = true
+		end
+	end
+	local playersTable = {}
+	local n = 0;
+	for key, _ in pairs(playersSet) do
+		playersTable[n] = key
+		n = n + 1;
+	end
+	local x = #playersTable;
+	local count;
+	local  i =0;
+	local y = 0;
+	while (seed <i )do
+		i = i + 1;
+		while (y < x) do
+			y = y +1;
+		end
+	end
+	return playersTable[y];
 end
 
 function playersAlive()
