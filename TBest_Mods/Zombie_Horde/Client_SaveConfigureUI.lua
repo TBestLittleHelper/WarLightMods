f079d256d9757b4992a8ec31fa7fc55ad0f71a32
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2017-05-22 19:57:43 +0200

    Update Client_SaveConfigureUI.lua

--- TBest_Mods/Zombie_Horde/Client_SaveConfigureUI.lua
+++ TBest_Mods/Zombie_Horde/Client_SaveConfigureUI.lua
@@ -3,4 +3,5 @@ function Client_SaveConfigureUI(alert)
     Mod.Settings.ExtraArmies = numberInputField1.GetValue();
     Mod.Settings.ZombieID = zombieInputField.GetValue();
     Mod.Settings.RandomZombie = RandomZombieBox.GetIsChecked();
+    Mod.Settings.ZombieWin = ZombieWinBox.GetIsChecked();
 end
