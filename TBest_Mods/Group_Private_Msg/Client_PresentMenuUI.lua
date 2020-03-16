25151f1d34f53b2e1fab74872718fcfa6fbed60e
TBestLittleHelper <tkjoita@gmail.com>
2020-03-16 10:48:45 +0100

    fix 2.0

--- TBest_Mods/Group_Private_Msg/Client_PresentMenuUI.lua
+++ TBest_Mods/Group_Private_Msg/Client_PresentMenuUI.lua
@@ -245,7 +245,11 @@ function CreateEditDialog(rootParent, setMaxSize, setScrollable, game, close)
 				temp = temp +1;				
 			end
 			temp = ClientGame.Us.ID .. '000' .. temp			
-			TargetGroupID = tonumber(temp) --NOTE! This gives us XXX.0 and not XXX in some cases. We can't index an array as XXX.0 , so this can give an error
+			--TargetGroupID = tonumber(temp) --NOTE! This gives us XXX.0 and not XXX in some cases. We can't index an array as XXX.0 , so this can give an error
+			TargetGroupID = tonumber(string.format("%." .. (0) .. "f", temp))
+			print(tonumber(string.format("%." .. (0) .. "f", temp)))
+			print("XXxxxxxxxxxxxXXXXXXXXXXx")
+
 			TargetGroupID = math.floor(TargetGroupID); -- Therfore, we also take the math.floor
 			print("made new groupID: " .. TargetGroupID)		
 			else
