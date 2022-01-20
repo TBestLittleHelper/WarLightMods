618df42eaedde0471467020b804a46a5556e3f55
TBestLittleHelper <tkjoita@gmail.com>
2022-01-21 00:17:39 +0100

    lint

--- TBest_Mods/City_Features/Server_StartDistribution.lua
+++ TBest_Mods/City_Features/Server_StartDistribution.lua
@@ -1,33 +1,36 @@
 --Called in any game set to manual territory distribution before players select their picks. This hook is not called in any game configured to automatic territory distribution.
-function Server_StartDistribution( game, standing)
-	if (Mod.Settings.Version ~= 1)then return end;
+function Server_StartDistribution(game, standing)
+	if (Mod.Settings.Version ~= 1) then
+		return
+	end
 
-    playerGameDataSetup(game, standing);
+	playerGameDataSetup(game, standing)
 end
 
 function playerGameDataSetup(game, standing)
 	--Set the mod boolean flag to be enabled
 	publicGameData = Mod.PublicGameData
-	publicGameData.GameFinalized = false;
-	publicGameData.Chat = {};
-	broadCastGroupSetup(game);
-	Mod.PublicGameData = publicGameData;
+	publicGameData.GameFinalized = false
+	publicGameData.Chat = {}
+	broadCastGroupSetup(game)
+	Mod.PublicGameData = publicGameData
 
-	playerGameData = Mod.PlayerGameData;
+	playerGameData = Mod.PlayerGameData
 
-	for _,pid in pairs(game.ServerGame.Game.Players)do
-		if(pid.IsAI == false)then
-			playerGameData[pid.ID] = {};
-			playerGameData[pid.ID].Chat = {}; -- For the chat function
+	for _, player in pairs(game.ServerGame.Game.Players) do
+		if (player.IsAI == false) then
+			playerGameData[player.ID] = {}
+			playerGameData[player.ID].Chat = {} -- For the chat function
 		end
 	end
-	Mod.PlayerGameData = playerGameData;
+	Mod.PlayerGameData = playerGameData
 end
 function broadCastGroupSetup(game)
-	publicGameData.Chat.BroadcastGroup = {};
-	publicGameData.Chat.BroadcastGroup[1] = "When a game ends, all chat messages will be made public. Also, check out settings and tweek it to your liking."
+	publicGameData.Chat.BroadcastGroup = {}
+	publicGameData.Chat.BroadcastGroup[1] =
+		"When a game ends, all chat messages will be made public. Also, check out settings and tweek it to your liking."
 	--publicGameData.Chat.BroadcastGroup[1].Sender = 0; --this might be someting we add in the future
-	publicGameData.Chat.BroadcastGroup[2] = "Note that messages to the server is rate-limited to 5 calls every 30 seconds per client. Therefore, do not spam chat or group changes: it won't work!"
+	publicGameData.Chat.BroadcastGroup[2] =
+		"Note that messages to the server are rate-limited. Therefore, do not spam chat messages or group changes: it won't work!"
 	publicGameData.Chat.BroadcastGroup.NumChat = 2
 end
-
