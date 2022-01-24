8616bddbbf6f14ece9b4082383f4697b8ec26e5d
TBestLittleHelper <tkjoita@gmail.com>
2022-01-24 22:13:26 +0100

    brief explain why mod is blocked

--- TBest_Mods/UndoTurn/Readme.md
+++ TBest_Mods/UndoTurn/Readme.md
@@ -1,1 +1,3 @@
 Requiers some changes to the mod framework before this can be fully done. But I put up the WIP code anyway.
+
+For example we can't write ActiveCards, Remove special units or getStanding from history (server side). Most blocking is active cards.
--- TBest_Mods/UndoTurn/Server_GameCustomMessage.lua
+++ TBest_Mods/UndoTurn/Server_GameCustomMessage.lua
@@ -10,9 +10,34 @@ function Server_GameCustomMessage(game, playerID, payload, setReturnTable)
 		Mod.PublicGameData = publicGameData
 		Dump(Mod.PublicGameData)
 		print(Mod.PublicGameData.CanUndoLastTurn[playerID])
+		Dump(game.ServerGame.LatestTurnStanding)
 	else
 		error("Payload message not understood (" .. payload.Message .. ")")
 	end
 end
 
--- TODO cooldown? Only allowe every other turn to be undone?
+--cooldown? Only let every other turn to be undone?
+
+function Dump(obj)
+	if obj.proxyType ~= nil then
+		DumpProxy(obj)
+	elseif type(obj) == "table" then
+		DumpTable(obj)
+	else
+		print("Dump " .. type(obj))
+	end
+end
+function DumpTable(tbl)
+	for k, v in pairs(tbl) do
+		print("k = " .. tostring(k) .. " (" .. type(k) .. ") " .. " v = " .. tostring(v) .. " (" .. type(v) .. ")")
+	end
+end
+function DumpProxy(obj)
+	print(
+		"type=" ..
+			obj.proxyType ..
+				" readOnly=" ..
+					tostring(obj.readonly) ..
+						" readableKeys=" .. table.concat(obj.readableKeys, ",") .. " writableKeys=" .. table.concat(obj.writableKeys, ",")
+	)
+end
