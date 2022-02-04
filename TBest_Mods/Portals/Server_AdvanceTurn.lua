2bed6f910df5583cbc25bef4c9356c09b17fea55
TBestLittleHelper <tkjoita@gmail.com>
2022-02-04 04:52:45 +0100

    Add portal mod.
    The mod swaps standing between the two portals

--- TBest_Mods/Portals/Server_AdvanceTurn.lua
+++ TBest_Mods/Portals/Server_AdvanceTurn.lua
@@ -1,2 +1,19 @@
 function Server_AdvanceTurn_End(game, addNewOrder)
+	TerritoryModifications = {}
+	Game = game
+
+	--Swap the standing (owner, armies) of the two portals
+	TerritoryModifications[1] = terrModHelper(Mod.PublicGameData.portalB, Mod.PublicGameData.portalA)
+	TerritoryModifications[2] = terrModHelper(Mod.PublicGameData.portalA, Mod.PublicGameData.portalB)
+
+	addNewOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral, "Portal Away", nil, TerritoryModifications, nil))
+end
+
+function terrModHelper(targertTerritory, sourceTerritory)
+	local terrMod = WL.TerritoryModification.Create(targertTerritory)
+
+	terrMod.SetArmiesTo = Game.ServerGame.LatestTurnStanding.Territories[sourceTerritory].NumArmies.NumArmies
+	terrMod.SetOwnerOpt = Game.ServerGame.LatestTurnStanding.Territories[sourceTerritory].OwnerPlayerID
+
+	return terrMod
 end
