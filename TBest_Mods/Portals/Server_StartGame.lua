8cc2215a655ae53c93c83374909378261643b547
TBestLittleHelper <tkjoita@gmail.com>
2022-02-04 04:52:45 +0100

    Add portal mod.
    The mod swaps standing between the two portals

--- TBest_Mods/Portals/Server_StartGame.lua
+++ TBest_Mods/Portals/Server_StartGame.lua
@@ -3,30 +3,23 @@ function Server_StartGame(game, standing)
 
 	territories = game.Map.Territories
 
-	portalA = math.randomchoice(territories)
-	territories[portalA.ID] = nil
-	portalB = math.randomchoice(territories)
+	local keys = {}
+	for key, value in pairs(territories) do
+		keys[#keys + 1] = key --Store keys in another table
+	end
+	index = keys[math.random(1, #keys - 1)]
 
-	print(portalA.Name)
-	print(portalB.Name)
-	publicGameData.portalA = portalA.ID
-	publicGameData.portalB = portalB.ID
+	portalA = territories[index].ID
+	portalB = territories[index + 1].ID
+
+	publicGameData.portalA = portalA
+	publicGameData.portalB = portalB
 
 	Mod.PublicGameData = publicGameData
 
 	local structure = {}
 	Portals = WL.StructureType.Power
 	structure[Portals] = 1
-	standing.Territories[portalA.ID].Structures = structure
-	standing.Territories[portalB.ID].Structures = structure
-end
-
---https://gist.github.com/jdev6/1e7ff30671edf88d03d4
-function math.randomchoice(t) --Selects a random item from a table
-	local keys = {}
-	for key, value in pairs(t) do
-		keys[#keys + 1] = key --Store keys in another table
-	end
-	index = keys[math.random(1, #keys)]
-	return t[index]
+	standing.Territories[portalA].Structures = structure
+	standing.Territories[portalB].Structures = structure
 end
