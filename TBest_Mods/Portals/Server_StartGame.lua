0bc5c45d9485b5c9349639993e549ac9b3d9f8fc
TBestLittleHelper <tkjoita@gmail.com>
2022-02-05 12:30:39 +0100

    fix territories random index

--- TBest_Mods/Portals/Server_StartGame.lua
+++ TBest_Mods/Portals/Server_StartGame.lua
@@ -1,16 +1,14 @@
 function Server_StartGame(game, standing)
-	publicGameData = Mod.PublicGameData
-
-	territories = game.Map.Territories
+	local publicGameData = Mod.PublicGameData
+	local territories = game.Map.Territories
 
 	local keys = {}
 	for key, value in pairs(territories) do
 		keys[#keys + 1] = key --Store keys in another table
 	end
-	index = keys[math.random(1, #keys - 1)]
-
-	portalA = territories[index].ID
-	portalB = territories[index + 1].ID
+	local index = math.random(1, #keys - 1)
+	portalA = territories[keys[index]].ID
+	portalB = territories[keys[index + 1]].ID
 
 	publicGameData.portalA = portalA
 	publicGameData.portalB = portalB
