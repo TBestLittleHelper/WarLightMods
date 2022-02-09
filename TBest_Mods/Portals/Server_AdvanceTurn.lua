0888c037b15efaf920d44401936c031982c69fc8
TBestLittleHelper <tkjoita@gmail.com>
2022-02-09 09:26:54 +0100

    Workaround for a bug in Warzone's javascript mod code.
    Just having the empty functions is a workaround. Can remove them after the website is updated

--- TBest_Mods/Portals/Server_AdvanceTurn.lua
+++ TBest_Mods/Portals/Server_AdvanceTurn.lua
@@ -1,3 +1,8 @@
+function Server_AdvanceTurn_Start(game, addNewOrder)
+end
+function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
+end
+
 function Server_AdvanceTurn_End(game, addNewOrder)
 	TerritoryModifications = {}
 	Game = game
