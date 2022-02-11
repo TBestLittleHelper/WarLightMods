28ea9113178819c99679a8d9ab02ebb606e4cb1e
TBestLittleHelper <tkjoita@gmail.com>
2022-02-11 12:50:50 +0100

    make sure numPortals is between 1 and 3

--- TBest_Mods/Portals/Client_SaveConfigureUI.lua
+++ TBest_Mods/Portals/Client_SaveConfigureUI.lua
@@ -1,3 +1,10 @@
 function Client_SaveConfigureUI(alert)
 	Mod.Settings.NumPortals = numberInputField.GetValue()
+	if (Mod.Settings.NumPortals < 1) then
+		Mod.Settings.NumPortals = 1
+	end
+
+	if (Mod.Settings.NumPortals > 3) then
+		Mod.Settings.NumPortals = 3
+	end
 end
