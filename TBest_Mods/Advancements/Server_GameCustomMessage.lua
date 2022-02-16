93f46fbef1861b329cb3b017c54e14f578b5e753
TBestLittleHelper <tkjoita@gmail.com>
2022-02-16 18:09:21 +0100

    Now updated unlockable status after it's bought.
    Also made the use of Capital letters more consistent

--- TBest_Mods/Advancements/Server_GameCustomMessage.lua
+++ TBest_Mods/Advancements/Server_GameCustomMessage.lua
@@ -3,19 +3,40 @@ require("Utilities")
 function Server_GameCustomMessage(game, playerID, payload, setReturnTable)
 	--payload = {key = key, TechTreeSelected = TechTreeSelected}
 
-	local playerGameData = Mod.PlayerGameData
 	local privateGameData = Mod.PrivateGameData
-	local publicGameData = Mod.PublicGameData
-
-	local unlockable = playerGameData[playerID].Advancment.Unlockables[payload.TechTreeSelected][payload.key]
+	local unlockable = privateGameData[playerID].Advancment.Unlockables[payload.TechTreeSelected][payload.key]
 	Dump(unlockable)
 	if
-		(unlockable.unlocked == false and
-			unlockable.preReq <= privateGameData[playerID].Advancment.PreReq[payload.TechTreeSelected])
+		(unlockable.Unlocked == false and
+			unlockable.PreReq <= privateGameData[playerID].Advancment.PreReq[payload.TechTreeSelected])
 	 then
-		if (unlockable.unlockPoints <= privateGameData[playerID].Advancment.Points[payload.TechTreeSelected]) then
-			print("We can buy it!")
+		if (unlockable.UnlockPoints <= privateGameData[playerID].Advancment.Points[payload.TechTreeSelected]) then
+			if (unlockable.Type == "Income" or unlockable.Type == "Attack" or unlockable.Type == "Defence") then
+				print("We can buy it!") -- TODO make this more readable
+				--Subtract the points from the techtree bank
+				privateGameData[playerID].Advancment.Points[payload.TechTreeSelected] =
+					privateGameData[playerID].Advancment.Points[payload.TechTreeSelected] - unlockable.UnlockPoints
+				--Set unlocked to true
+				privateGameData[playerID].Advancment.Unlockables[payload.TechTreeSelected][payload.key].Unlocked = true
+				--Add one the the PreReq counter
+				privateGameData[playerID].Advancment.PreReq[payload.TechTreeSelected] =
+					privateGameData[playerID].Advancment.PreReq[payload.TechTreeSelected] + 1
+
+				if (privateGameData[playerID].Bonus[unlockable.Type] == nil) then
+					privateGameData[playerID].Bonus[unlockable.Type] = 0
+				end
+				privateGameData[playerID].Bonus[unlockable.Type] =
+					privateGameData[playerID].Bonus[unlockable.Type] + unlockable.Power
+
+				Mod.PrivateGameData = privateGameData
+				--If not an AI, sync to playerGameData
+				if not (game.ServerGame.Game.Players[playerID].IsAI) then
+					local playerGameData = Mod.PlayerGameData
+					playerGameData[playerID] = Mod.PrivateGameData[playerID]
+					Mod.PlayerGameData = playerGameData
+				end
+				return
+			end
 		end
 	end
-	--		{AttackBoost = 10, unlockPoints = 10, preReq = 0, unlocked = false, text = "Increase offensive kill rate by 10"},
 end
