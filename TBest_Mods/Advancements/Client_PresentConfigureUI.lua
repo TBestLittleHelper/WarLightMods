1d863a2d1532f2a818ea1cbe0fbeca25c4987297
TBestLittleHelper <tkjoita@gmail.com>
2022-02-17 13:30:27 +0100

    Add modsettings to enable/disable a tech tree

---
+++ TBest_Mods/Advancements/Client_PresentConfigureUI.lua
@@ -0,0 +1,21 @@
+function Client_PresentConfigureUI(rootParent)
+	local initialAdvancments = Mod.Settings.Advancment
+	if initialAdvancments == nil then
+		initialAdvancments = {}
+	end
+	if initialAdvancments.Technology == nil then
+		initialAdvancments.Technology = true
+	end
+	if initialAdvancments.Military == nil then
+		initialAdvancments.Military = true
+	end
+	if initialAdvancments.Culture == nil then
+		initialAdvancments.Culture = true
+	end
+
+	local mainContainer = UI.CreateVerticalLayoutGroup(rootParent)
+	UI.CreateLabel(mainContainer).SetText("Select the Advancments trees to be incuded")
+	TechnologyCheckBox = UI.CreateCheckBox(mainContainer).SetText("Technology").SetIsChecked(initialAdvancments.Technology)
+	MilitaryCheckBox = UI.CreateCheckBox(mainContainer).SetText("Military").SetIsChecked(initialAdvancments.Military)
+	CultureCheckBox = UI.CreateCheckBox(mainContainer).SetText("Culture").SetIsChecked(initialAdvancments.Culture)
+end
