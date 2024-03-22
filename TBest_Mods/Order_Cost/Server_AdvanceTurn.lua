2b0c5ed07c93a0e4a4f53c515788511259a19904
TBest <TBestLittleHelper@users.noreply.github.com>
2024-03-22 16:09:30 +0100

    remove debug

--- TBest_Mods/Order_Cost/Server_AdvanceTurn.lua
+++ TBest_Mods/Order_Cost/Server_AdvanceTurn.lua
@@ -4,17 +4,12 @@ local playersTable = {}
 
 function Server_AdvanceTurn_Start(game, addNewOrder)
 	playersTable = {}
-	print("Start of Server_AdvanceTurn_Start: " .. tostring(playersTable));
 	for playerID, _ in pairs(game.ServerGame.Game.PlayingPlayers) do
 		playersTable[playerID] = Mod.Settings.FreeOrders
 	end
-	print("End of Server_AdvanceTurn_Start: " .. tostring(playersTable));
 end
 
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
-	if (playersTable == nil) then
-		print("`playersTable` is nil! " .. tostring(playersTable))
-	end
 	--We skip game order event's, since we create that order ourselves. This does argubly harm mod compatabilety.
 	if (order.proxyType == "GameOrderEvent")then return end
 
@@ -24,7 +19,6 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 end
 
 function Server_AdvanceTurn_End(game, addNewOrder)
-	print("Start of Server_AdvanceTurn_End: " .. tostring(playersTable));
 	for playerID, cost in pairs(playersTable) do
 		if (cost > 0) then
 			cost = 0
@@ -33,5 +27,4 @@ function Server_AdvanceTurn_End(game, addNewOrder)
 		local incomeMod = WL.IncomeMod.Create(playerID, cost, "Cost of orders")
 		addNewOrder(WL.GameOrderEvent.Create(playerID, cost .. " cost of orders", nil, {}, nil, {incomeMod}))
 	end
-	print("End of Server_AdvanceTurn_End: " .. tostring(playersTable));
 end
