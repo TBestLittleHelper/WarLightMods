3e08fa30b706b42d59b734fe8afcfefbe92ba762
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2024-03-22 01:02:43 +0100

    Update Server_AdvanceTurn.lua

--- TBest_Mods/Order_Cost/Server_AdvanceTurn.lua
+++ TBest_Mods/Order_Cost/Server_AdvanceTurn.lua
@@ -2,18 +2,23 @@
 
 function Server_AdvanceTurn_Start(game, addNewOrder)
 	playersTable = {}
+	print("Start of Server_AdvanceTurn_Start: " .. tostring(playersTable));
 	for playerID, _ in pairs(game.ServerGame.Game.PlayingPlayers) do
 		playersTable[playerID] = Mod.Settings.FreeOrders
 	end
+	print("End of Server_AdvanceTurn_Start: " .. tostring(playersTable));
 end
 
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
+	print("Start of Server_AdvanceTurn_Order: " .. tostring(playersTable));
 	--We skip game order event's, since we create that order ourselves. This does argubly harm mod compatabilety.
 	if (order.proxyType == "GameOrderEvent")then return end
 
+	print("After GameOrderEvent check: " .. tostring(playersTable));
 	if (playersTable[order.PlayerID] ~= nil)then
 		playersTable[order.PlayerID] = playersTable[order.PlayerID] -1
 	end
+	print("End of Server_AdvanceTurn_Order: " .. tostring(playersTable));
 end
 
 function Server_AdvanceTurn_End(game, addNewOrder)
